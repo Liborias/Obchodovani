7d66ae8f75b370f179378523520f63d29a62e199
Libor Žák <libor.zak.zn@gmail.com>
2020-09-25 16:34:38 +0200

    Material design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-                <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>
+          Obchodování
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button color="primary">Hello World</Button>
       </header>
     </div>
   );
