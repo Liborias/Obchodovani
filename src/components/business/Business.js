065dbaf2acb9c232c504167a6cc8f497c8fcd5c6
Libor Žák <libor.zak.zn@gmail.com>
2020-10-05 11:21:58 +0200

    sdfs

---
+++ src/components/business/Business.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Layout from "../layout/Layout";
+
+const Business = () => (
+  <>
+
+    <h1>Portfolio</h1>
+  </>
+);
+
+export default Business;
