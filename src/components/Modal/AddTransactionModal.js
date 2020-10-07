3f1d92fe781105c7d7cfba0312da6923cf606e6f
Zakor <libor.zak.zn@gmail.com>
2020-10-08 00:07:21 +0200

    Podmíněné vyplnění, datum atd.

--- src/components/Modal/AddTransactionModal.js
+++ src/components/Modal/AddTransactionModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -11,12 +11,60 @@ import InputLabel from '@material-ui/core/InputLabel';
 import "./Modals.css";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 
 export default function AddTransactionModal(props) {
 
-  const [newRow, setNewRow] = React.useState(
+  const [isTickerValid, setTickerValidity] = useState(false);
+  const [isLongevityValid, setLongevityValidity] = useState(false);
+  const [isAmountValid, setAmountValidity] = useState(false);
+  const [isPriceValid, setPriceValidity] = useState(false);
+
+
+  const validateTicker = (ticker) => {
+    setTickerValidity(true);
+    if (!ticker.includes("")) {
+      setTickerValidity(false);
+    }
+  };
+
+  const validateLongevity = (longevity) => {
+    setLongevityValidity(true);
+    if (!longevity.includes("")) {
+      setLongevityValidity(false);
+    }
+  };
+
+  const validateAmount = (amount) => {
+    setAmountValidity(true);
+    if (!amount > 0) {
+      setAmountValidity(false);
+    }
+  };
+  const validatPrice = (price) => {
+    setPriceValidity(true);
+    if (!price > 0) {
+      setPriceValidity(false);
+    }
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+  }));
+
+  const classes = useStyles();
+
+  const [newRow, setNewRow] = useState(
     {
       id: Math.round(Math.random() * 1000000000),
       companyName: '',
@@ -31,12 +79,12 @@ export default function AddTransactionModal(props) {
   );
 
   const handleChange = (event) => {
-    setNewRow({ ...newRow, [event.target.name]: event.target.checked });
+    const freeRideLabel = event.target.checked ? "ANO" : "NE";
+    setNewRow({ ...newRow, [event.target.name]: event.target.checked, freeRideLabel: freeRideLabel });
   };
 
-  const onClickSave = () => {
-    props.handleSave(newRow);
-    newRow.freeRide ? setNewRow({ ...newRow, freeRide: "ANO" }) : setNewRow({ ...newRow, freeRide: "NE" });
+  const onClickAct = () => {
+    newRow.freeRide ? setNewRow({ ...newRow, freeRideLabel: "ANO" }) : setNewRow({ ...newRow, freeRideLabel: "NE" });
   };
 
   return (
@@ -44,6 +92,7 @@ export default function AddTransactionModal(props) {
       <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
         <DialogTitle id="formDialogTitle">Nový obchod</DialogTitle>
         <DialogContent>
+
           <div className="tradeSelectors">
             <div className="particularSelector">
               <InputLabel id="tickerLabel">Zkratka společnosti</InputLabel>
@@ -55,7 +104,8 @@ export default function AddTransactionModal(props) {
                   const selectedCompany = props.companies.find((company) => ticker === company.ticker)
 
 
-                  setNewRow({ ...newRow, shortcut: ticker, companyName: selectedCompany.companyName })
+                  setNewRow({ ...newRow, shortcut: ticker, companyName: selectedCompany.companyName });
+                  validateTicker(e.target.value);
                 }}
               >{props.companies.map((company) => {
                 return < MenuItem value={company.ticker}>{company.ticker}</MenuItem>
@@ -71,7 +121,11 @@ export default function AddTransactionModal(props) {
               <Select
                 labelId="longevityLabel"
                 id="longevitySelect"
-                onChange={(e) => setNewRow({ ...newRow, longevity: e.target.value })}
+                onChange={(e) => {
+                  setNewRow({ ...newRow, longevity: e.target.value });
+                  validateLongevity(e.target.value);
+                }
+                }
               >
                 <MenuItem value={"Plovoucí krátkodobá"}>Plovoucí krátkodobá</MenuItem>
                 <MenuItem value={"Pevná střednědobá"}>Pevná střednědobá</MenuItem>
@@ -95,6 +149,21 @@ export default function AddTransactionModal(props) {
             />
 
           </div>
+
+          <TextField
+            autoFocus
+            id="date"
+            label="Datum transakce"
+            type="date"
+            defaultValue="Datum transakce"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setNewRow({ ...newRow, buyDate: e.target.value })}
+          />
+
+
           <TextField
             autoFocus
             margin="dense"
@@ -113,7 +182,10 @@ export default function AddTransactionModal(props) {
             label="Množství akcií"
             type="number"
             fullWidth
-            onChange={(e) => setNewRow({ ...newRow, amount: e.target.value })}
+            onChange={(e) => {
+              setNewRow({ ...newRow, amount: e.target.value });
+              validateAmount(e.target.value)
+            }}
           />
           <TextField
             autoFocus
@@ -122,14 +194,22 @@ export default function AddTransactionModal(props) {
             label="Cena 1. akcie"
             type="number"
             fullWidth
-            onChange={(e) => setNewRow({ ...newRow, stockPrice: e.target.value })}
+            onChange={(e) => {
+              setNewRow({ ...newRow, stockPrice: e.target.value });
+              validatPrice(e.target.value)
+            }}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={props.handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={onClickSave} color="primary">
+          <Button type="submit" disabled={!isTickerValid || !isLongevityValid || !isAmountValid || !isPriceValid} onClick={() => {
+            onClickAct();
+            props.handleSave(newRow);
+          }
+          }
+            color="primary">
             Save
           </Button>
         </DialogActions>
